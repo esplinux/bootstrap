f290185b87c1fb4e874077d280d81ffba0998b72
Eric Molitor <eric@molitor.org>
2020-02-02 19:33:55 +0000

    Add shadow utils to fix alpine azure container for bootstrap

--- scripts/build-azure-alpine.sh
+++ scripts/build-azure-alpine.sh
@@ -2,7 +2,7 @@
 
 cnt=$(buildah from alpine:edge)
 buildah run $cnt -- sh -c "echo 'http://dl-cdn.alpinelinux.org/alpine/edge/testing' >> /etc/apk/repositories"
-buildah run $cnt apk add bash sudo nodejs
+buildah run $cnt apk add bash sudo nodejs shadow
 buildah config --label com.azure.dev.pipelines.agent.handler.node.path=/usr/bin/node $cnt
 buildah config --cmd node $cnt
 buildah commit --squash $cnt esplinux/alpine-azurepipeline
