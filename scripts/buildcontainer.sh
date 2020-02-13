ede33be96bb5c7305d5173fd000e6e7a6507a554
Eric Molitor <eric@molitor.org>
2020-02-13 08:15:49 +0000

    Debug buildah

--- scripts/buildcontainer.sh
+++ scripts/buildcontainer.sh
@@ -6,17 +6,17 @@ TOP=$(pwd)
 
 echo Building container $CONTAINER_NAME CMD $CMD
 
-buildah --name $CONTAINER_NAME from scratch
-mnt=$(buildah unshare buildah mount $CONTAINER_NAME)
+buildah --storage-driver=vfs --name $CONTAINER_NAME from scratch
+mnt=$(buildah --storage-driver=vfs mount $CONTAINER_NAME)
 cd $mnt
 for tarball in "$@"
 do
   tar xf $TOP/$tarball-*.tgz
 done
-buildah unshare buildah unmount $CONTAINER_NAME
-buildah unshare buildah config --cmd $CMD $CONTAINER_NAME
+buildah --storage-driver=vfs unmount $CONTAINER_NAME
+buildah --storage-driver=vfs config --cmd $CMD $CONTAINER_NAME
 
 # Hack label for Azure Pipelines into all containers
-buildah unshare buildah config --label com.azure.dev.pipelines.agent.handler.node.path=/usr/local/bin/node $CONTAINER_NAME
+buildah --storage-driver=vfs config --label com.azure.dev.pipelines.agent.handler.node.path=/usr/local/bin/node $CONTAINER_NAME
 
-buildah unshare buildah commit --squash $CONTAINER_NAME $CONTAINER_NAME
+buildah --storage-driver=vfs commit --squash $CONTAINER_NAME $CONTAINER_NAME
