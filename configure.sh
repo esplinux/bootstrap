83f2706225e475a7d604a47cb219ba58d5d69110
Eric Molitor <eric@molitor.org>
2020-03-08 12:15:21 +0000

    New Baseline Bootstrap
    * Fix linux-headers include paths
    * Fix ldflags in most locations (llvm pending)

--- configure.sh
+++ configure.sh
@@ -7,7 +7,7 @@ TOYBOX_version=toybox-HEAD
 CMAKE_version=cmake-3.16.5
 AWK_version=awk-HEAD
 SBASE_version=sbase-HEAD
-DASH_version=dash-0.5.10
+#DASH_version=dash-0.5.10
 VIM_version=vim-8.2.0347
 BEARSSL_version=bearssl-HEAD
 CURL_version=curl-7.69.0
@@ -16,9 +16,10 @@ CURSES_version=netbsd-curses-HEAD
 ZLIB_version=zlib-1.2.11
 SAMURAI_version=samurai-HEAD
 GETTEXT_version=gettext-tiny-HEAD
+ZSH_version=zsh-5.8
 GIT_version=git-2.25.1
 GNUMAKE_version=make-3.81
-GNUBASH_version=bash-3.2.57
+#GNUBASH_version=bash-3.2.57
 
 check()
 {
@@ -56,6 +57,9 @@ else
   LLVM_CCACHE_BUILD=ON
 fi
 
+BEARSSL_headers=$PWD/$BEARSSL_version/inc
+BEARSSL_lib=$PWD/out-$LINUX_version
+LINUX_headers=$PWD/out-$LINUX_version/include
 SYSROOT=$PWD/sysroot
 HOSTROOT=$PWD/build-host-$LLVM_version
 MARCH=broadwell
@@ -64,6 +68,7 @@ LLVM_TBLGEN=$PWD/build-host-$llvm/bin/llvm-tblgen
 CLANG_TBLGEN=$PWD/build-host-$llvm/bin/clang-tblgen
 CFLAGS="-Os -pipe -march=$MARCH -mtune=$MARCH"
 CXXFLAGS="-Os -pipe -march=$MARCH -mtune=$MARCH"
+LDFLAGS="-w -s"
 
 if ! test -f "config.ninja"; then
   echo "Creating config.ninja"
@@ -77,18 +82,21 @@ if ! test -f "config.ninja"; then
   echo "cmake=$CMAKE_version" >> config.ninja
   echo "awk=$AWK_version" >> config.ninja
   echo "sbase=$SBASE_version" >> config.ninja
-  echo "dash=$DASH_version" >> config.ninja
+#  echo "dash=$DASH_version" >> config.ninja
   echo "vim=$VIM_version" >> config.ninja
   echo "bearssl=$BEARSSL_version" >> config.ninja
+  echo "bearssl-headers=$BEARSSL_headers" >> config.ninja
+  echo "bearssl-lib=$BEARSSL_lib" >> config.ninja
   echo "curl=$CURL_version" >> config.ninja
   echo "byacc=$BYACC_version" >> config.ninja
   echo "curses=$CURSES_version" >> config.ninja
   echo "zlib=$ZLIB_version" >> config.ninja
   echo "gettext=$GETTEXT_version" >> config.ninja
+  echo "zsh=$ZSH_version" >> config.ninja
   echo "git=$GIT_version" >> config.ninja
   echo "samurai=$SAMURAI_version" >> config.ninja
   echo "gnumake=$GNUMAKE_version" >> config.ninja
-  echo "gnubash=$GNUBASH_version" >> config.ninja
+#  echo "gnubash=$GNUBASH_version" >> config.ninja
   echo "sysroot=$SYSROOT" >> config.ninja
   echo "march=$MARCH" >> config.ninja
   echo "nproc=$NPROC" >> config.ninja
@@ -101,6 +109,8 @@ if ! test -f "config.ninja"; then
   echo "yacc=$SYSROOT/bin/yacc" >> config.ninja
   echo "llvm-tblgen=$HOSTROOT/bin/llvm-tblgen" >> config.ninja
   echo "clang-tblgen=$HOSTROOT/bin/clang-tblgen" >> config.ninja
+  echo "linux-headers=$LINUX_headers" >> config.ninja
   echo "cflags=$CFLAGS" >> config.ninja
   echo "cxxflags=$CXXFLAGS" >> config.ninja
+  echo "ldflags=$LDFLAGS" >> config.ninja
 fi
